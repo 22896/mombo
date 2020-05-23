d349caa40a767a4df8874d1d6993881509f8f391
simjaeseon <359248@naver.com>
2020-05-23 13:15:31 +0900

    로그인

--- app/src/main/java/com/example/mombo/registration.java
+++ app/src/main/java/com/example/mombo/registration.java
@@ -1,19 +1,19 @@
 package com.example.mombo;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.Bundle;
+
+
 import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -22,31 +22,30 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 
-
-public class MainActivity extends AppCompatActivity {
+public class registration extends AppCompatActivity {
     private EditText emailTV, passwordTV;
-    private Button loginBtn;
+    private Button regBtn;
     private ProgressBar progressBar;
 
     private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.registration);
 
         mAuth = FirebaseAuth.getInstance();
 
         initializeUI();
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginUserAccount();
+                registerNewUser();
             }
         });
     }
 
-    private void loginUserAccount() {
+    private void registerNewUser() {
         progressBar.setVisibility(View.VISIBLE);
 
         String email, password;
@@ -62,19 +61,19 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        mAuth.signInWithEmailAndPassword(email, password)
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_LONG).show();
                             progressBar.setVisibility(View.GONE);
 
-                            Intent intent = new Intent(MainActivity.this, FirstActivity.class);
+                            Intent intent = new Intent(registration.this, MainActivity.class);
                             startActivity(intent);
                         }
                         else {
-                            Toast.makeText(getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
                             progressBar.setVisibility(View.GONE);
                         }
                     }
@@ -84,9 +83,9 @@ public class MainActivity extends AppCompatActivity {
     private void initializeUI() {
         emailTV = findViewById(R.id.email);
         passwordTV = findViewById(R.id.password);
-
-        loginBtn = findViewById(R.id.login);
+        regBtn = findViewById(R.id.register);
         progressBar = findViewById(R.id.progressBar);
     }
 }
+
 
