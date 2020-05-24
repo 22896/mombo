7db910c49b11c5ae0be136b2c3cafcdcd6c81a21
22896 <22896yes@gmail.com>
2020-05-25 00:44:24 +0900

    레이아웃 거지같은데 급해서 그냥 올림

--- app/src/main/java/com/example/mombo/alarm.java
+++ app/src/main/java/com/example/mombo/alarm.java
@@ -2,13 +2,29 @@ package com.example.mombo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class alarm extends AppCompatActivity {
+ImageButton back_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
+        back_btn = (ImageButton) findViewById(R.id.back);
+
+        ImageButton Button1 = (ImageButton) findViewById(R.id.back);
+        Button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View w) {
+                Intent intent = new Intent(getApplicationContext(), FirstActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
